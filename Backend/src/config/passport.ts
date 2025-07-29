f8cfbab690d219aa5311f1f90539047ee725abd3
badalsah <badalsah8541@gmail.com>
2025-07-29 17:14:51 +0530

    updated passport

--- Backend/src/config/passport.ts
+++ Backend/src/config/passport.ts
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: '/api/auth/google/callback',
+      callbackURL: 'https://note-taking-backend-roh4.onrender.com/api/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
